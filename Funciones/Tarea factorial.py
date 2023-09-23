c8ad12ffec8d035c309a513f010c71ca027b6925
CarolDayanReyes <142327900+CarolDayanReyes@users.noreply.github.com>
2023-09-22 22:54:14 -0500

    Tarea factorial Final.py

--- Funciones/Tarea factorial.py
+++ Funciones/Tarea factorial.py
@@ -1,15 +1,11 @@
-def detector_factorial(factorial):
-    return factorial
-
-while True:
-
-    detector_factorial = int(input("Enter a positive integer value: "))
-    print("Valor: ", detector_factorial)
-    a = isinstance(detector_factorial, int)
-    if a == True and detector_factorial > 0:
-        fact = 1
-        for i in range (1, detector_factorial + 1):
-            fact = fact*i            
-        print(f'The factorial of {detector_factorial} is: ', fact)
-    else:
-        print("Please, enter a positive integer number")
+while True: 
+     def procFact(factorial): 
+         if factorial==1: 
+             return 1 
+         else: 
+             return factorial * procFact(factorial-1) 
+     factorial=int(input('Digite el límite del rango: ')) 
+     print(procFact(factorial)) 
+     y=int(input("Para finalizar digite el número 1 y para continuar cualquier otro número entero: ")) 
+     if y==1: 
+         break
