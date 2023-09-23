543a6052abbd8c9b8aedcd6a1ba313658ed422d1
CarolDayanReyes <142327900+CarolDayanReyes@users.noreply.github.com>
2023-09-22 23:01:42 -0500

    Actualizar TareaPrimos.py

--- Funciones/TareaPrimos.py
+++ Funciones/TareaPrimos.py
@@ -1,31 +1,19 @@
-def detector_primos(primos):
-    return primos
-
-p = 1
-detector_primos = int(input('\nIngrese un valor para saber si es primo: '))
-print("\n")
-
-while p == 1:
-    for i in range(1,detector_primos+1):
-        conta = 0
-        for n in range(1, i+1):
-            residue = i%n
-            if residue == 0:
-                conta = conta + 1
-            
-    if conta == 2:
-       print(f'{i} efectivamente es un primo.')
-       print("\n")
-    else:
-       print(f'{i} lastimosamente no es un primo.')
-       print("\n")
-
-    print('¿Deseas averiguar si otro número es primo?. Escribe 1 si la respuesta es afirmativa de lo contrario envía cualquier otro NÚMERO.')
-    p = int(input())
-
-    if p != 1:
-        break
-
-    detector_primos = int(input('\nIngrese un valor para saber nuevamente si es primo: '))
-    print("\n")
-    
+while True:    
+     def procPrimo(primo): 
+         print('1 efectivamente es primo') 
+         for i in range(2,primo+1): 
+             conta = 0 
+             for n in range(2, primo+1): 
+                 residue = i%n 
+                 if residue == 0: 
+                     conta = conta + 1 
+             if conta == 1: 
+                         print(f'{i} efectivamente es primo') 
+             else: 
+                         print(f'{i} NO es primo') 
+         return "¡RANGO FINALIZADO!"  
+     primo=int(input('Ingrese el rango hasta el cuál desea saber los números primos: ')) 
+     print(procPrimo(primo)) 
+     y=int(input("Para continuar digite cualquier número entero y para finalizar digite el número 1: ")) 
+     if y==1: 
+         break
